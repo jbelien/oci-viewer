11f174f537081fb9356f6e3ac549021ef82ff224
Jonathan Beliën <dev@jbelien.be>
2020-05-30 21:35:30 +0200

    Update _layout.svelte
    Get and OCI version.

--- src/routes/_layout.svelte
+++ src/routes/_layout.svelte
@@ -1,9 +1,24 @@
+<script context="module">
+    import Map from "../components/Map.svelte";
+
+    export async function preload({ params }) {
+        const res = await this.fetch("oci.json");
+        const { version } = await res.json();
+
+        if (res.status === 200) {
+            return { version };
+        } else {
+            this.error(res.status);
+        }
+    }
+</script>
+
 <script>
     import Nav from '../components/Nav.svelte';
     import Footer from '../components/Footer.svelte';
 
     export let segment;
-    export let version = "0.0.0";
+    export let version;
 </script>
 
 <style>
